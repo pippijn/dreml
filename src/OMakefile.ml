855a474ca8d5a46ae32cc631456a9f6dda615f3b
Pippijn van Steenhoven <pip88nl@gmail.com>
2013-07-05 13:32:16 +0200

    use mlmake preprocessor

---
+++ src/OMakefile.ml
@@ -0,0 +1,42 @@
+install Program ".DEFAULT" [
+  (* Target *)
+  Name		"dreml";
+
+  (* Sources *)
+  Modules [
+    "Dreml";
+    "Duplicates";
+    "ExprSets";
+    "Instruction";
+    "Language";
+    "Lexer";
+    "Main";
+    "Nfa";
+    "Ops";
+    "Parser";
+    "Partial";
+    "Pattern";
+    "Simplify";
+    "Transition";
+    "Types";
+    "Util";
+    "Visualise";
+  ];
+
+  (* Library dependencies *)
+  OCamlRequires [
+    "baselib";
+    "batteries.syntax";
+    "deriving-ocsigen";
+    "deriving-ocsigen.syntax";
+  ];
+
+  (* Camlp4 *)
+  Flags [
+    "main.ml",		"-syntax camlp4o";
+    "nfa.ml",		"-syntax camlp4o";
+    "exprSets.ml",	"-syntax camlp4o";
+    "partial.ml",	"-syntax camlp4o";
+    "types.ml",		"-syntax camlp4o";
+  ];
+]
