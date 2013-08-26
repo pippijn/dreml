2e995ce051d65388589fae207eae39852eec52e8
Pippijn van Steenhoven <pip88nl@gmail.com>
2013-08-26 04:10:38 +0200

    reimplement BatList.unique_cmp
    to take the first rather than the last unique element

--- src/duplicates.ml
+++ src/duplicates.ml
@@ -1,3 +1,22 @@
+let rec unique_cmp uniq seen cmp l =
+  match l with
+  | x :: xs ->
+      if BatMap.mem x seen then (
+        unique_cmp uniq seen cmp xs
+      ) else (
+        let seen = BatMap.add x () seen in
+        unique_cmp (x :: uniq) seen cmp xs
+      )
+  | [] ->
+      List.rev uniq
+      
+
+
+let unique_cmp ?(cmp=compare) l =
+  let seen = BatMap.create cmp in
+  unique_cmp [] seen cmp l
+
+
 (* Keep all matches. *)
 let remove_duplicate_results in_nfa states =
   let states =
@@ -14,9 +33,9 @@ let remove_duplicate_results in_nfa states =
       ) states
   in
 
-  BatList.unique (
+  unique_cmp (
     List.map (fun (state, env) ->
-      state, BatList.unique env
+      state, unique_cmp env
     ) states
   )
 
@@ -37,4 +56,4 @@ let remove_duplicate_results in_nfa states =
       ) states
   in
 
-  BatList.unique ~cmp:(fun (state1, _) (state2, _) -> state1 = state2) states
+  unique_cmp ~cmp:(fun (state1, _) (state2, _) -> compare state1 state2) states
